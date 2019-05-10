5ea0c20713fec01b59cb43e144e56a2059109519
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-05-11 04:49:03 +0530

    taskmanagement

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+let id=1;
+export function uniqueID() {
+  return ++id  ;
+}
+
+export function createTask({title,description}){
+
+    return{
+        type:'CREATE_TASK',
+        payload:{
+            id:uniqueID(),
+            title,
+            description,
+            status:'Unstarted'
+        }
+    }
+}
+
+export function editTask(id,param={}){
+    return{
+        type:'EDIT_TASK',
+        payload:{
+            id,
+            param
+        }
+
+    }
+}
