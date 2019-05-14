b29cc2149c3bbbe82127e799bc5676e6685c60e4
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-05-14 14:45:59 +0530

    TaskManagementApp

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,50 +1,51 @@
-import React  from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-const styles = {
-    card: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  };
+import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Typography from "@material-ui/core/Typography";
+import ListItemText from "@material-ui/core/ListItemText";
+import Button from "@material-ui/core/Button";
+
+const TASK_STATUSES = ["Unstarted", "In Progress", "Completed"];
+
+export default function Task(props) {
+  const { status, tasks ,onEdit} = props;
   
-
-const TASK_STATUSES=['Unstarted','In Progress','Completed'];
-
-const Task=props=>{
-    const { classes } = props;
-  function onStatusChange(e){
-      props.onStatusChange(
-       e.target.id,
-       e.target.value
-      )
-  }
-return(
-    <div className='task'>
-    <div className="task-header">
-    <div>{props.task.title}</div>
-    
-    <select value={props.task.status}  id={props.task.id} onChange={(e)=>onStatusChange(e)}>
-     {TASK_STATUSES.map(status=><option key={status} value={status}>{status}</option>)}
-    </select>
+  return (
+    <div>
+      <List>
+        {tasks!=null&&tasks.map(
+          i =>
+            i.status == status && (
+              <ListItem button>
+                <ListItemText
+                  inset
+                  primary={
+                    <Typography
+                      variant="h4"
+                      align="left"
+                      component="h2"
+                      color="textPrimary"
+                    >
+                      {i.title}
+                    </Typography>
+                  }
+                  secondary={
+                    <Typography
+                      variant="h5"
+                      component="div"
+                      color="textPrimary"
+                    >
+                      {i.description}
+                    </Typography>
+                  }
+                />
+                <select value={i.status} id={i.id} onChange={(e)=>onEdit(e)}>
+             {       TASK_STATUSES.map(status=><option key={status} value={status}>{status}</option>)}
+              </select>
+              </ListItem>
+            )
+        )}
+      </List>
     </div>
-    <hr/>
-    <div className="task-body">{props.task.description}</div>
-    </div>
-)
-
+  );
 }
-export default withStyles(styles)(Task)
