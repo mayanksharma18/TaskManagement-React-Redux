416586780b09d52b512e87a489d27d0888efbd35
mayanksharma18 <mmayank.sharma18@gmail.com>
2019-05-14 14:40:15 +0530

    TaskManagement

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,43 +1,24 @@
 
-import { uniqueID } from '../actions/index';
-
-
-
-const mockTasks = [
-    // {
-    //   id: uniqueID(),
-    //   title: 'Learn Redux',
-    //   description: 'The store, actions, and reducers, oh my!',
-    //   status: 'In Progress',
-    // },
-    // {
-    //   id: uniqueID(),
-    //   title: 'Peace on Earth',
-    //   description: 'No big deal.',
-    //   status: 'In Progress',
-    // },
-  ];
-export default function tasks(state={tasks:mockTasks},action){
-    const {payload}=action
-    console.log(payload)
-    switch(action.type){
+const tasks=[]
+export default function task(state={tasks},action){
+   const {payload}=action
+   console.log(payload)
+     switch(action.type){
         case 'CREATE_TASK':
         return {
-            tasks:state.tasks.concat(action.payload)
+            tasks:state.tasks.concat(payload)
         }
         case 'EDIT_TASK':
         return{
             tasks:state.tasks.map(task=>{
                 if(task.id==parseInt(payload.id)){
-                   console.log("Enter")
-                    return {...task,status:payload.param}
+                    return {...task,status:payload.status}
                 }
                 return task
             })
         }
         default:
-        return state;
+        return state
     }
-
     
 }
